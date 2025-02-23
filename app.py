7d9a70089c0ae13a59a923cc4ed656fb3817a958
Eric Li <eli303@gatech.edu>
2025-02-23 08:38:14 -0500

    8:38

--- app.py
+++ app.py
@@ -2,15 +2,6 @@
 import streamlit as st
 import tempfile
 import subprocess
-import sys
-def install_system_dependencies():
-    try:
-        subprocess.check_call(["apt-get", "update"])
-        subprocess.check_call(["apt-get", "install", "-y", "libsm6", "libxext6", "libgl1-mesa-glx", "ffmpeg"])
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while installing system dependencies: {e}")
-        sys.exit(1)
-install_system_dependencies()
 import cv2
 import os
 import time
